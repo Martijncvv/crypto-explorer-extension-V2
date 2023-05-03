8b7e4cc9fd484ce3e7b7543236ce61e5f1bfd9bc
martijn <martijncvv@gmail.com>
2023-05-03 05:24:53 +0200

    - added social blocks - added start of exchangeblock

---
+++ dist/colors.tsx
@@ -0,0 +1,24 @@
+interface Colors {
+    primary_medium: string;
+    primary_dark: string;
+
+    secondary_medium: string;
+    accent_medium: string;
+
+    red_medium: string;
+    green_medium: string;
+    white_medium: string;
+}
+
+const colors: Colors = {
+    primary_medium: '#34495e',
+    primary_dark: '#2F396D',
+
+    secondary_medium: '#8F95EC',
+    accent_medium: '#FFFFFF80',
+    red_medium: '#FD8E8E',
+    green_medium: '#88EE58',
+    white_medium: '#F5F5F5',
+};
+
+export default colors;
