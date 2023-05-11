831d1d241f1a743a7a8ebd85b2a780268743da82
martijn <martijncvv@gmail.com>
2023-05-11 05:00:31 +0200

    - added a few variables and Interfaces

--- dist/popup.js
+++ dist/popup.js
@@ -1202,48 +1202,51 @@ const styles = {
 const App = () => {
     var _a;
     const [coinInfo, setCoinInfo] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)();
+    console.log("coinInfo1: ", coinInfo);
+    // TODO fix conditional rendering
     return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { style: styles.container },
         react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_HeaderBlock__WEBPACK_IMPORTED_MODULE_8__["default"], { mainLogo: ((_a = coinInfo === null || coinInfo === void 0 ? void 0 : coinInfo.image) === null || _a === void 0 ? void 0 : _a.small) ? coinInfo.image.small : blockchainIcon, setCoinInfo: setCoinInfo }),
         react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ChartsBlock__WEBPACK_IMPORTED_MODULE_9__["default"], { pricedata: [], volumedata: [] }),
-        react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_PriceBar__WEBPACK_IMPORTED_MODULE_3__["default"], { allTimeLow: 0.22, allTimeHigh: 1.78, price: 1.60 }),
-        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { style: styles.dataBlocks },
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ValueBlock__WEBPACK_IMPORTED_MODULE_4__["default"], { title: "Circ. Supply", mainValue: "139 T", secondaryValue: "/ 250 T" }),
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ValueBlock__WEBPACK_IMPORTED_MODULE_4__["default"], { title: "Market Cap", mainValue: "$11.6 B", secondaryValue: "#9" })),
-        react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ExchangeBlock__WEBPACK_IMPORTED_MODULE_7__["default"], { exchanges: [
-                {
-                    image: redditIcon,
-                    exchangeName: "WOOnetwork",
-                    tradingVolume: "$2.6 B",
-                    websiteLink: "https://www.reddit.com/r/dogecoin/",
-                },
-                {
-                    image: discordIcon,
-                    exchangeName: "Binance",
-                    tradingVolume: "$13.4 M",
-                    websiteLink: "https://www.reddit.com/r/dogecoin/",
-                },
-                {
-                    image: twitterIcon,
-                    exchangeName: "Huobi",
-                    tradingVolume: "$1.4 M",
-                    websiteLink: "https://www.reddit.com/r/dogecoin/",
-                },
-                {
-                    image: coingeckoIcon,
-                    exchangeName: "Bittrex",
-                    tradingVolume: "$0.5 M",
-                    websiteLink: "https://www.reddit.com/r/dogecoin/",
-                },
-            ] }),
-        react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ExpandableTextField__WEBPACK_IMPORTED_MODULE_5__["default"], { text: "Dogecoin is a cryptocurrency based on the popular \"Doge\" Internet meme and features a Shiba Inu on its logo. Dogecoin is a Litecoin fork. Introduced as a \"joke currency\" on 6 December 2013, Dogecoin quickly developed its own online community and reached a capitalization of US$60 million in January 2014. Compared with other cryptocurrencies, Dogecoin had a fast initial coin production schedule: 100 billion coins were in circulation by mid-2015, with an additional 5.256 billion coins every year thereafter. As of 30 June 2015, the 100 billionth Dogecoin had been mined. Dogecoin was created by Billy Markus from Portland, Oregon and Jackson Palmer from Sydney, Australia. Both wanted to create a fun cryptocurrency that will appeal beyond the core Bitcoin audience. Dogecoin is primarily used as a tipping system on Reddit and Twitter where users tip each other for creating or sharing good content. The community is very active in organising fundraising activities for deserving causes. The developers of Dogecoin haven’t made any major changes to the coin since 2015. This means that Dogecoin could get left behind and is why Shibas are leaving Dogecoin to join more advanced platforms like Ethereum. One of Dogecoin strengths is its relaxed and fun-loving community. However, this is also a weakness because other currencies are way more professional. To purchase Dogecoin, it involves downloading a crypto wallet, setting up a crypto exchange account and then trading away for your desired crypto currency. Once we have set up an account with a DOGE currency exchange and deposited some funds, you are ready to start trading." }),
-        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { style: styles.socialBlocks },
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: websiteIcon }),
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: blockchainIcon }),
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: coingeckoIcon, mainValue: "9 M" }),
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: twitterIcon, mainValue: "1.7 K" }),
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: redditIcon, mainValue: "14 M" }),
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: telegramIcon, mainValue: "10 K" }),
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: discordIcon }))));
+        (coinInfo === null || coinInfo === void 0 ? void 0 : coinInfo.name) &&
+            react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null,
+                react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_PriceBar__WEBPACK_IMPORTED_MODULE_3__["default"], { allTimeLow: coinInfo.market_data.atl.usd, allTimeHigh: coinInfo.market_data.ath.usd, price: coinInfo.market_data.current_price.usd }),
+                react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { style: styles.dataBlocks },
+                    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ValueBlock__WEBPACK_IMPORTED_MODULE_4__["default"], { title: "Circ. Supply", mainValue: coinInfo.market_data.circulating_supply.toString(), secondaryValue: `/ ${coinInfo.market_data.total_supply}` }),
+                    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ValueBlock__WEBPACK_IMPORTED_MODULE_4__["default"], { title: "Market Cap", mainValue: coinInfo.market_data.market_cap.toString(), secondaryValue: coinInfo.market_cap_rank.toString() })),
+                react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ExchangeBlock__WEBPACK_IMPORTED_MODULE_7__["default"], { exchanges: [
+                        {
+                            image: redditIcon,
+                            exchangeName: "WOOnetwork",
+                            tradingVolume: "$2.6 B",
+                            websiteLink: "https://www.reddit.com/r/dogecoin/",
+                        },
+                        {
+                            image: discordIcon,
+                            exchangeName: "Binance",
+                            tradingVolume: "$13.4 M",
+                            websiteLink: "https://www.reddit.com/r/dogecoin/",
+                        },
+                        {
+                            image: twitterIcon,
+                            exchangeName: "Huobi",
+                            tradingVolume: "$1.4 M",
+                            websiteLink: "https://www.reddit.com/r/dogecoin/",
+                        },
+                        {
+                            image: coingeckoIcon,
+                            exchangeName: "Bittrex",
+                            tradingVolume: "$0.5 M",
+                            websiteLink: "https://www.reddit.com/r/dogecoin/",
+                        },
+                    ] }),
+                react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_ExpandableTextField__WEBPACK_IMPORTED_MODULE_5__["default"], { text: coinInfo.description }),
+                react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { style: styles.socialBlocks },
+                    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: websiteIcon, link: coinInfo.links.homepage[0] }),
+                    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: blockchainIcon, link: coinInfo.links.blockchain_site[0] }),
+                    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: coingeckoIcon, mainValue: "9 M", link: `https://www.coingecko.com/en/coins/${coinInfo.id}` }),
+                    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: twitterIcon, mainValue: "1.7 K", link: `https://twitter.com/${coinInfo.links.twitter_screen_name}` }),
+                    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: redditIcon, mainValue: "14 M", link: coinInfo.links.subreddit_url }),
+                    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_SocialBlock__WEBPACK_IMPORTED_MODULE_6__["default"], { image: telegramIcon, mainValue: "10 K", link: `https://t.me/${coinInfo.links.telegram_channel_identifier}` })))));
 };
 const root = document.createElement('div');
 document.body.appendChild(root);
@@ -1379,7 +1382,7 @@ function fetchCoinInfo(coinId) {
     return __awaiter(this, void 0, void 0, function* () {
         coinId = coinId || 'bitcoin';
         try {
-            const res = yield fetch(`https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`);
+            const res = yield fetch(`https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&market_data=true&community_data=false&developer_data=false&sparkline=false`);
             if (!res.ok) {
                 throw new Error(`Fetch error, coin info data (${coinId}): ${res.status} ${res.statusText}`);
             }
