66ec42e4291af869bf2653b15eaa6302f3b45fb2
martijn <martijncvv@gmail.com>
2023-12-16 05:02:24 +0100

    - implemented caching for all major fetches -> improved API efffiency with a factor 3/ 4

--- dist/service_worker.js
+++ dist/service_worker.js
@@ -1,1 +1,88 @@
-function sleep(e){return new Promise((t=>setTimeout(t,e)))}(async()=>{chrome.runtime.onInstalled.addListener(((e,t,s)=>{chrome.alarms.create("trackAccountsAlarm",{delayInMinutes:1,periodInMinutes:60})})),chrome.alarms.onAlarm.addListener((async e=>{if("trackAccountsAlarm"===e.name){const{trackedAccounts:e}=await chrome.storage.local.get("trackedAccounts");if(e?.length>0)for(const t of e)try{await sleep(5e3);const s=await fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=${t.address}&startblock=0&endblock=99999999&page=1&offset=10&sort=desc`);if(!s.ok){console.log(`Fetch error, ${t.address} fetchLatestAddressTxs txs info: ${s.status} ${s.statusText}`);continue}const a=await s.json();if(a.result.length>0){const s=a.result[0].nonce;if(t.nonce!==s){const n=parseInt(s)-parseInt(t.nonce)||1,o=1===n?`${t.name} has ${n} new transaction`:`${t.name} has ${n} new transactions`,r=a.result[0].value||"0",c=BigInt(r)/BigInt(10**18),i=c>0?`Just sent ${c} ETH`:"No ETH sent";chrome.notifications.create({type:"basic",iconUrl:"/images/CryptoExplorer_logo_128.png",title:"Crypto Tracker",message:`${o}\n                  ${i}`}),chrome.storage.local.set({trackedAccounts:e.map((e=>e.address===t.address?{...e,nonce:s,lastUpdated:Date.now()}:e))})}}}catch(e){console.error("Error fetching address tx info:",e)}}}))})();
+(async () => {
+  chrome.runtime.onInstalled.addListener((message, sender, sendResponse) => {
+    createAlarm();
+  });
+
+  function createAlarm() {
+    chrome.alarms.create("trackAccountsAlarm", {
+      delayInMinutes: 1,
+      periodInMinutes: 60,
+    });
+  }
+
+  chrome.alarms.onAlarm.addListener(async (alarm) => {
+    if (alarm.name === "trackAccountsAlarm") {
+      const { trackedAccounts: accounts } = await chrome.storage.local.get(
+        "trackedAccounts",
+      );
+
+      if (accounts?.length > 0) {
+        for (const accountInfo of accounts) {
+          try {
+            await sleep(5200); // to prevent hitting API limit
+
+            const res = await fetch(
+              `https://api.etherscan.io/api?module=account&action=txlist&address=${accountInfo.address}&startblock=0&endblock=99999999&page=1&offset=3&sort=desc`,
+            );
+            if (!res.ok) {
+              console.log(
+                `Fetch error, ${accountInfo.address} fetchLatestAddressTxs txs info: ${res.status} ${res.statusText}`,
+              );
+              continue;
+            }
+            const response = await res.json();
+
+            if (response.result.length > 0) {
+              const latestNonce = response.result[0].nonce;
+
+              if (accountInfo.nonce !== latestNonce) {
+                const txDifference =
+                  parseInt(latestNonce) - parseInt(accountInfo.nonce);
+                const txText =
+                  txDifference === 1
+                    ? `${accountInfo.name} has ${txDifference} new transaction`
+                    : `${accountInfo.name} has ${txDifference} new transactions`;
+
+                const weiValue = response.result[0].value || "0";
+                const ethValue = BigInt(weiValue) / BigInt(10 ** 18);
+                const ethText = ethValue > 0 ? `Just sent ${ethValue} ETH` : "";
+                chrome.notifications.create({
+                  type: "basic",
+                  iconUrl: "/images/CryptoExplorer_logo_128.png",
+                  title: `Crypto Tracker`,
+                  message: `${txText}
+                  ${ethText}`,
+                });
+
+                chrome.storage.local.set({
+                  trackedAccounts: accounts.map((account) => {
+                    if (account.address === accountInfo.address) {
+                      return {
+                        ...account,
+                        nonce: latestNonce,
+                        lastUpdated: Date.now(),
+                      };
+                    }
+                    return account;
+                  }),
+                });
+              }
+              // else {
+              // console.log("No new txs for tracked accounts");
+              // }
+            }
+          } catch (error) {
+            console.error("Error fetching address tx info:", error);
+          }
+        }
+      }
+      // else {
+      // console.log("No tracked accounts");
+      // }
+    }
+  });
+})();
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
